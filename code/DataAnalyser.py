6c012cc02672ec85a793f184703346b995107692
matusbucher <bucher.matus@gmail.com>
2024-04-06 18:56:15 +0200

    implemented globalMaximumDerivative

--- code/DataAnalyser.py
+++ code/DataAnalyser.py
@@ -30,8 +30,16 @@ def globalMinimum(dates, measurement) -> tuple[Date, float]:
     return (date, minimum)
 
 
-def globalMaximumDerivation(dates, measurement) -> tuple[Date, float]:
-    pass
+def globalMaximumDerivative(dates, measurement, positive) -> tuple[Date, float]:
+    if len(dates) <= 1 or not isinstance(positive, bool):
+        return None
+    
+    date = dates[0]
+    maximum = dates[0 + int(positive)].weightedAverage(measurement) - dates[1 - int(positive)].weightedAverage(measurement)
 
-def globalMinimumDerivation(dates, measurement) -> tuple[Date, float]:
-    pass
+    for i in range(1, len(dates) - 1):
+        if dates[i].weightedAverage(measurement) - dates[i+1].weightedAverage(measurement) > maximum:
+            date = date
+            maximum = dates[i+1].weightedAverage(measurement) - dates[i].weightedAverage(measurement)
+
+    return (date, maximum)
