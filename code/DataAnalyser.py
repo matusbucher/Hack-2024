24d36eb7606b553efd16e6e9fb5ec35b49aaaa00
matusbucher <bucher.matus@gmail.com>
2024-04-06 23:58:59 +0200

    add tests and fix bugs

--- code/DataAnalyser.py
+++ code/DataAnalyser.py
@@ -1,22 +1,21 @@
 from Date import *
 from math import sqrt
+from typing import *
 
-def globalMaximum(dates, measurement) -> tuple[Date, float]:
+def globalMaximum(dates: Collection[Date], measurement: str) -> tuple[Date, float]:
     if len(dates) == 0:
         return None
     
     date = dates[0]
     maximum = dates[0].weightedAverage(measurement)
-
     for d in dates[1:]:
         if d.weightedAverage(measurement) > maximum:
             date = d
             maximum = d.weightedAverage(measurement)
-
     return (date, maximum)
 
 
-def globalMinimum(dates, measurement) -> tuple[Date, float]:
+def globalMinimum(dates: Collection[Date], measurement: str) -> tuple[Date, float]:
     if len(dates) == 0:
         return None
     
@@ -31,40 +30,42 @@ def globalMinimum(dates, measurement) -> tuple[Date, float]:
     return (date, minimum)
 
 
-def globalMaximumDerivative(dates, measurement, positive) -> tuple[Date, float]:
+def globalMaximumDerivative(dates: Collection[Date], measurement: str, positive: bool) -> tuple[Date, float]:
     if len(dates) <= 1 or not isinstance(positive, bool):
         return None
     
     date = dates[0]
     maximum = dates[0 + int(positive)].weightedAverage(measurement) - dates[1 - int(positive)].weightedAverage(measurement)
-
     for i in range(1, len(dates) - 1):
-        if dates[i].weightedAverage(measurement) - dates[i+1].weightedAverage(measurement) > maximum:
-            date = date
-            maximum = dates[i+1].weightedAverage(measurement) - dates[i].weightedAverage(measurement)
-
+        if dates[i + int(positive)].weightedAverage(measurement) - dates[i+1 - int(positive)].weightedAverage(measurement) > maximum:
+            date = dates[i]
+            maximum = dates[i + int(positive)].weightedAverage(measurement) - dates[i+1 - int(positive)].weightedAverage(measurement)
     return (date, maximum)
 
-def correlationCoefficient(date_1, date_2, measurement_1, measurement_2) -> float:
-    date_1_avg = date_1.weightedAverage(measurement_1)
-    date_2_avg = date_2.weightedAverage(measurement_2)
-    start_year = 2000
-    end_year = 2023
-    # Nepytaj sa
-    return sum([(date_1.data[start_year + i][measurement_1] - date_1_avg) * (date_2.data[start_year + i][measurement_2] - date_2_avg) for i in range(end_year - start_year + 1)]) / ((sqrt(sum([(date_1.data[start_year + i][measurement_1] - date_1_avg)**2 for i in range(end_year - start_year + 1)]))) * (sqrt(sum([(date_2.data[start_year + i][measurement_2] - date_2_avg)**2 for i in range(end_year - start_year + 1)]))))
+def correlationCoefficient(date_1: Date, date_2: Date, measurement_1: str, measurement_2: str) -> float:
+    years = set(date_1.data.keys()).intersection(date_2.data.keys())
 
-def biggestCorrelationCoefficient(date, dates, *measurements) -> tuple[str, float]:
-    biggest = ("None", 0)
-    for m in measurements:
-        for mm in measurements:
-            for day in dates:
-                if day != date:
-                    c = correlationCoefficient(date, day, m, mm)
-                    if c > biggest[1]:
-                        biggest = (f"{day} {m} {mm}", c)
+    if (len(years) == 0):
+        return None
+    
+    date_1_avg = date_1.averageFromYears(measurement_1, years)
+    date_2_avg = date_2.averageFromYears(measurement_2, years)
+    date_1_diffs = [(date_1.data[year][measurement_1] - date_1_avg) for year in years]
+    date_2_diffs = [(date_2.data[year][measurement_2] - date_2_avg) for year in years]
+    numerator = sum([date_1_diffs[i] * date_2_diffs[i] for i in range(len(years))])
+    denominator = sqrt(sum(diff**2 for diff in date_1_diffs) * sum(diff**2 for diff in date_2_diffs))
+    
+    if denominator == 0:
+        return 0
+    return numerator / denominator
+
+def biggestCorrelationCoefficient(date: Date, dates: Collection[Date], *measurements: str) -> tuple[Date, str, str, float]:
+    biggest = (None, "-", "-", 0)
+    for d in dates:
+        if d != date:
+            for m1 in measurements:
+                for m2 in measurements:
+                    r = correlationCoefficient(date, d, m1, m2)
+                    if abs(r) > abs(biggest[3]):
+                        biggest = (d, m1, m2, r)
     return biggest
-
-
-
-
-
--- code/tests.py
+++ code/tests.py
@@ -1,10 +1,10 @@
 from Date import *
+from DataAnalyser import *
 import unittest
 
 class TestDateMethods(unittest.TestCase):
 
-    def test_loadData(self):
-        data = {"01.01.":{2000:{"cloud_cover":10,
+    data = {"01.01.":{2000:{"cloud_cover":34,
                               "temperature":5,
                               "wind_speed":32,
                               "rain_mm":69,
@@ -16,38 +16,139 @@ class TestDateMethods(unittest.TestCase):
                               "snow_mm":20},
                         2002:{"cloud_cover":21,
                               "temperature":9.8,
-                              "wind_speed":0,
+                              "wind_speed":3,
                               "rain_mm":0,
                               "snow_mm":0}},
-                "02.01.":{2000:{"cloud_cover":0,
-                              "temperature":0,
-                              "wind_speed":0,
+                "02.01.":{2000:{"cloud_cover":98,
+                              "temperature":0.01,
+                              "wind_speed":33,
                               "rain_mm":0,
                               "snow_mm":0},
-                        2002:{"cloud_cover":0,
-                              "temperature":0,
-                              "wind_speed":0,
+                        2002:{"cloud_cover":75,
+                              "temperature":-5,
+                              "wind_speed":13,
                               "rain_mm":0,
-                              "snow_mm":0}},
-                "03.01.":{2000:{"cloud_cover":0,
+                              "snow_mm":125}},
+                "03.01.":{2000:{"cloud_cover":66,
+                              "temperature":3.2,
+                              "wind_speed":64,
+                              "rain_mm":17,
+                              "snow_mm":0},
+                        2001:{"cloud_cover":9,
                               "temperature":0,
-                              "wind_speed":0,
+                              "wind_speed":25,
                               "rain_mm":0,
                               "snow_mm":0},
-                        2001:{"cloud_cover":0,
-                              "temperature":0,
-                              "wind_speed":0,
-                              "rain_mm":0,
-                              "snow_mm":0},
-                        2002:{"cloud_cover":0,
-                              "temperature":0,
-                              "wind_speed":0,
-                              "rain_mm":0,
+                        2002:{"cloud_cover":42,
+                              "temperature":1.4,
+                              "wind_speed":61,
+                              "rain_mm":420,
                               "snow_mm":0}}}
-        date = Date(1, 1, 2000, 2002)
-        date.loadData(data, "cloud_cover", "temperature", "wind_speed", "rain_mm", "snow_mm")
+    
+    measurements = ("cloud_cover", "temperature", "wind_speed", "rain_mm", "snow_mm")
 
-        self.assertEqual(data["01.01."], date.data)
+    def test_loadData(self):
+        date0101 = Date(1, 1, 2000, 2002)
+        date0101.loadData(self.data, *self.measurements)
+        date0201 = Date(2, 1, 2000, 2002)
+        date0201.loadData(self.data, *self.measurements)
+        date0301 = Date(3, 1, 2000, 2002)
+        date0301.loadData(self.data, *self.measurements)
+        self.assertEqual(self.data["01.01."], date0101.data)
+        self.assertEqual(self.data["02.01."], date0201.data)
+        self.assertEqual(self.data["03.01."], date0301.data)
+    
+    def test_average(self):
+        date = Date(1, 1, 2000, 2002)
+        date.loadData(self.data, *self.measurements)
+        self.assertEqual(round(date.average("temperature"), 2), 3.77)
+        self.assertEqual(round(date.weightedAverage("temperature"), 2), 4.3)
+    
+    def test_standardDeviation(self):
+        date = Date(1, 1, 2000, 2002)
+        date.loadData(self.data, *self.measurements)
+        self.assertEqual(round(date.variance("temperature"), 2), 30.24)
+        self.assertEqual(round(date.standardDeviation("temperature"), 2), 5.5)
+
+# end TestDateMethods
+
+
+class TestDataAnalyserFunctions(unittest.TestCase):
+
+    data = {"01.01.":{2000:{"cloud_cover":34,
+                              "temperature":5,
+                              "wind_speed":32,
+                              "rain_mm":69,
+                              "snow_mm":0},
+                        2001:{"cloud_cover":8,
+                              "temperature":-3.5,
+                              "wind_speed":15,
+                              "rain_mm":0,
+                              "snow_mm":20},
+                        2002:{"cloud_cover":21,
+                              "temperature":9.8,
+                              "wind_speed":3,
+                              "rain_mm":0,
+                              "snow_mm":0}},
+                "02.01.":{2000:{"cloud_cover":98,
+                              "temperature":0.01,
+                              "wind_speed":33,
+                              "rain_mm":0,
+                              "snow_mm":0},
+                        2002:{"cloud_cover":75,
+                              "temperature":-5,
+                              "wind_speed":13,
+                              "rain_mm":0,
+                              "snow_mm":125}},
+                "03.01.":{2000:{"cloud_cover":66,
+                              "temperature":3.2,
+                              "wind_speed":64,
+                              "rain_mm":17,
+                              "snow_mm":0},
+                        2001:{"cloud_cover":9,
+                              "temperature":0,
+                              "wind_speed":25,
+                              "rain_mm":0,
+                              "snow_mm":0},
+                        2002:{"cloud_cover":42,
+                              "temperature":1.4,
+                              "wind_speed":61,
+                              "rain_mm":420,
+                              "snow_mm":0}}}
+    
+    measurements = ("cloud_cover", "temperature", "wind_speed", "rain_mm", "snow_mm")
+
+    dates = [Date(1, 1, 2000, 2002), Date(2, 1, 2000, 2002), Date(3, 1, 2000, 2002)]
+    for date in dates:
+        date.loadData(data, *measurements)
+
+    def test_globalMaximumAndMinimum(self):
+        ret1 = globalMaximum(self.dates, "temperature")
+        self.assertEqual(ret1[0], self.dates[0])
+        self.assertEqual(round(ret1[1], 2), 4.3)
+
+        ret2 = globalMinimum(self.dates, "temperature")
+        self.assertEqual(ret2[0], self.dates[1])
+        self.assertEqual(round(ret2[1], 2), -4)
+
+    def test_globalMaximumDerivative(self):
+        ret1 = globalMaximumDerivative(self.dates, "wind_speed", True)
+        self.assertEqual(ret1[0], self.dates[1])
+        self.assertEqual(round(ret1[1], 2), 26.07)
+
+        ret2 = globalMaximumDerivative(self.dates, "cloud_cover", False)
+        self.assertEqual(ret2[0], self.dates[1])
+        self.assertEqual(round(ret2[1], 2), 62.87)
+
+    def test_correlationCoefficient(self):
+        ret1 = correlationCoefficient(self.dates[0], self.dates[2], "temperature", "temperature")
+        self.assertEqual(round(ret1, 2), 0.57)
+
+        ret2 = biggestCorrelationCoefficient(self.dates[0], self.dates, *self.measurements)
+        print(ret2)
+
+# end TestDataAnalyserFunctions
+
 
 if __name__ == '__main__':
     unittest.main()
